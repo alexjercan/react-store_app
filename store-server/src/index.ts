c97031a63d04f0196f2d09dbba8c20d24f007248
Alexandru Jercan <jercan_alex27@yahoo.com>
2020-09-03 21:25:07 +0300

    Create backend
    Send fetch request to kaufland stores api

--- store-server/src/index.ts
+++ store-server/src/index.ts
@@ -1,10 +1,38 @@
 import express from 'express';
+import dotenv from 'dotenv';
+import fetch from 'node-fetch';
+
+dotenv.config();
+
+const kauflandApiURL = 'https://api.kaufland.net/stores/api/v1/';
 
 const app = express();
 const port = 8080;
 
+let stores = [{}];
+
+const getStores = async() => {
+    try {
+        const response = await fetch(kauflandApiURL, {
+            method: 'GET',
+            headers: {'Ocp-Apim-Subscription-Key': `${process.env.KAUFLAND_API_KEY}`}
+        });
+
+        const body = await response.json();
+        return body;
+    }
+    catch(err) {
+        console.log(err);
+    }
+}
+
+getStores().then((data) => {
+    stores = data;
+});
+
 app.get( "/", ( req, res ) => {
-    res.send( "Hello world!" );
+    console.log(stores);
+    res.send(stores);
 } );
 
 app.listen(port);
