16ff3e59461575f30594a4fcb062fd7ceffa4ac2
Alexandru Jercan <jercan_alex27@yahoo.com>
2020-09-04 13:58:25 +0300

    Add prettier

--- store-app/src/components/Store.tsx
+++ store-app/src/components/Store.tsx
@@ -1,19 +1,11 @@
-import React from 'react';
+import React from "react";
 
 interface Props {
-    name: string
+  name: string;
 }
 
 const Store: React.FC<Props> = (props) => {
-    return (
-        <li>
-            {
-                props.name
-            }
-        </li>
-    );
+  return <li>{props.name}</li>;
 };
 
 export default Store;
-
-
--- store-app/src/components/StoreList.tsx
+++ store-app/src/components/StoreList.tsx
@@ -1,58 +1,58 @@
-import React, { useEffect, useState } from 'react';
-import Store from './Store';
+import React, { useEffect, useState } from "react";
+import Store from "./Store";
 
 interface Store {
-    id: string
-    name: string
+  id: string;
+  name: string;
 }
 
 interface Coordinates {
-    latitude: number
-    longitude: number
+  latitude: number;
+  longitude: number;
 }
 
 const StoreList: React.FC = () => {
-    const [stores, setStores] = useState<Store[]>([]);
-    const [position, setPosition] = useState<Coordinates | undefined>(undefined);
+  const [stores, setStores] = useState<Store[] | null>();
+  const [position, setPosition] = useState<Coordinates | null>();
 
-    useEffect(() => {
-    }, []);
+  useEffect(() => {
+    const getStores = async () => {
+      const response = await fetch("http://localhost:8080/", {
+        method: "GET",
+      });
 
-    useEffect(() => {
-        const getStores = async () => {
-            const response = await fetch('http://localhost:8080/', {
-                method: 'GET'
-            });
+      if (response.status !== 400) {
+        const dataArray: any[] = await response.json();
+        const storesArray = dataArray.map(
+          (store): Store => {
+            return { id: store.storeId, name: store.name };
+          }
+        );
+        setStores(storesArray);
+      }
+    };
 
-            if (response.status !== 400) {
-                const dataArray: any[] = await response.json();
-                const storesArray = dataArray.map((store): Store => {
-                    return {id: store.storeId, name: store.name};
-                });
-                setStores(storesArray);
-            }
-        }
+    navigator.geolocation.getCurrentPosition((position) => {
+      setPosition({
+        latitude: position.coords.latitude,
+        longitude: position.coords.longitude,
+      });
+    });
 
-        navigator.geolocation.getCurrentPosition((position) => {
-            setPosition({latitude: position.coords.latitude, longitude: position.coords.longitude});
-        });
+    getStores();
+  }, []);
 
-        getStores();
-    }, []);
-    
-    console.log(position);
+  console.log(position);
 
-    return (
-        <div>
-            <ul>
-                {
-                    stores.map((store) => (
-                        <Store key={store.id} name={store.name}/>
-                    ))
-                }
-            </ul>
-        </div>
-    );
-}
+  return (
+    <div>
+      <ul>
+        {stores?.map((store) => (
+          <Store key={store.id} name={store.name} />
+        ))}
+      </ul>
+    </div>
+  );
+};
 
 export default StoreList;
